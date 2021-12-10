3d89409f96f0b28900908acf1ea28a56d372c020
amor da sua vida <94574738+prdzin777@users.noreply.github.com>
2021-12-10 15:59:26 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 var imagemDaTorre, torre;
 var imgFantasma, fantasma;
 var imgDoor;
-var imgclimber
+var imgclimber;
 
 
 function preload() {
@@ -14,17 +14,19 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  torre = createSprite(windowWidth, windowHeight);
+  torre = createSprite(windowWidth / 2, windowHeight / 2);
   torre.addImage("tower", imagemDaTorre);
   torre.velocityY = 1;
+  torre.scale = 3.5;
 
-  fantasma = createSprite(windowWidth, windowHeight);
+  fantasma = createSprite(windowWidth / 2, windowHeight / 2);
   fantasma.addImage("fantasma", imgFantasma);
-  fantasma.scale = 0.35;
+  fantasma.scale = 0.55;
 
-  
+
   grupoPorta = new Group();
-  
+  grupoClimber = new Group();
+
 
 }
 
@@ -32,49 +34,61 @@ function setup() {
 function draw() {
   drawSprites();
 
-  if (torre.y >= height){
-    torre.y = height/2;
+  if (torre.y >= height) {
+    torre.y = height / 6;
 
   }
-  coordenadas();
 
-  if (keyDown("left")){
+
+  if (keyDown("left")) {
     fantasma.x = fantasma.x - 5;
   }
 
-  if (keyDown("right")){
-    fantasma.x +=  5;
+  if (keyDown("right")) {
+    fantasma.x += 5;
   }
 
-  if ((touches.length > 0 || keyDown("space")){
+  if (touches.length > 0 || keyDown("space")) {
     fantasma.velocityY = -10;
   }
+             
   
+  if (grupoClimber.isTouching(fantasma)){
+    fantasma.velocityY = 0
+  }
   fantasma.velocityY = fantasma.velocityY + 0.8;
-  
+
   geraPorta();
 
-  
+
 
 
 }
 
-function coordenadas(){
+function coordenadas() {
   fill("yellow");
   textSize(20);
-  text(mouseX+","+mouseY, mouseX, mouseY);
+  text(mouseX + "," + mouseY, mouseX, mouseY);
 
 }
 
-function geraPorta(){
-  if(frameCount % 240 === 0){
-    var porta = createSprite(random(windowWidth, windowHeight), 0);
-    var climber = createSprite(porta.x, porta.y);
+function geraPorta() {
+  if (frameCount % 500 === 0) {
+    var porta = createSprite(random(100, windowWidth - 100), 0);
+    var climber = createSprite(porta.x, porta.y + 100);
+    
     porta.velocityY = torre.velocityY;
     climber.velocityY = porta.velocityY;
-    porta.addimage = (imgDoor);
-    climber.addimage = (imgClimber);
+    porta.addImage("door", imgDoor);
+    climber.addImage("climber", imgclimber);
+    porta.scale = 1.5;
+    climber.scale = 1.50;
+    porta.depth = fantasma.depth -1;
+    climber.depth = porta.depth;
+    grupoPorta.add(porta);
+    grupoClimber.add(climber);
   }
-  
 
-}
+     
+
+}          
